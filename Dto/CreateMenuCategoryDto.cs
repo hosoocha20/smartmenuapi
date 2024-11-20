e0505f306a0db49353c06a146c2d1903c6f188fd
Hosoo Cha <hosoo.cha@gmail.com>
2024-11-20 15:51:12 +1300

    Add CreateMenuCategoryDto

---
+++ Dto/CreateMenuCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartMenuManagerApp.Dto
+{
+    public class CreateMenuCategoryDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
